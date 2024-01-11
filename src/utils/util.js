cee57f98bf50b00f16fa8fe32951b91f9ed3e71c
AiurRefugee <2298641141@qq.com>
2024-01-11 15:37:32 +0800

    copy

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,19 +1,32 @@
 
 export function copyToClipboard(text) {
-  navigator.clipboard.writeText(text)
-    .then(() => {
-      ElMessage({
-        type: 'success',
-        message: '复制成功',
-        grouping: true,
+  try {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        ElMessage({
+          type: 'success',
+          message: '复制成功',
+          grouping: true,
+        })
+        console.log('Text copied to clipboard:', text);
+        // 可以在这里执行成功复制到剪贴板后的操作
       })
-      console.log('Text copied to clipboard:', text);
-      // 可以在这里执行成功复制到剪贴板后的操作
+      .catch(err => {
+
+        ElMessage({
+          type: 'error',
+          message: error,
+          grouping: true,
+        })
+        // 复制失败时的处理
+      });
+  } catch (err) {
+    ElMessage({
+      type: 'error', 
+      message: err,
+      grouping: true,
     })
-    .catch(err => {
-      console.error('Error copying text:', err);
-      // 复制失败时的处理
-    });
+  }
 }
 
 export let adding = false
